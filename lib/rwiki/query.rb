19cf733469b0be73ab5abe49464b01fe5403f1b0
yuji takahashi <jit.y.31@gmail.com>
2017-02-26 20:26:46 +0900

    Modify Query

--- lib/rwiki/query.rb
+++ lib/rwiki/query.rb
@@ -1,11 +1,13 @@
 module Rwiki
   class Query
-    DEFAULT_PARAMS = { format: :xml,action: :query }.freeze
+    using Rwiki::Extension
+    DEFAULT_PARAMS = { format: :xml, action: :query }.freeze
+    attr_reader :params
+
     def self.build(&block)
       new(&block).to_query
     end
-    using Rwiki::Extension
-    attr_reader :params
+
     def initialize(&block)
       @params = Openstruct.new(DEFAULT_PARAMS)
       block.call(@params)
